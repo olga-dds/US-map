ef1b2572b5467c29b5e5ad44c791624ace4e2a73
Olga Radan <olga@dotdesignstudio.ro>
2019-09-04 14:30:25 +0300

    add api directory

--- assets/js/uStates.js
+++ assets/js/uStates.js
@@ -151,8 +151,8 @@ var offersCoordinates = [
 ]
 
  var offersData = []
-
 manipulateOffersData()
+init()
 
 //LEGEND DATA
 
@@ -269,6 +269,29 @@ manipulateOffersData()
 	
 	})
 	}
+
+	function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', '.api/coordinates-data.json', true);
+    // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState === 4 && xobj.status === "200") {
+            // Required use of an anonymous callback 
+            // as .open() will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+function init() {
+    loadJSON(function(response) {
+        // Parse JSON string into object
+				var actual_JSON = JSON.parse(response);
+				console.log("coordinates-data",actual_JSON)
+    });
+}
 })();
 
 
